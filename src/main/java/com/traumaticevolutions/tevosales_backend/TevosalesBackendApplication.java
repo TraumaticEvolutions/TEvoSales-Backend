1292882fef44fc42acc40a6e67361b73c84def7d
Algol95 <angelaragon.developer@gmail.com>
2025-04-01 23:47:57 +0200

    feat(TEVO-7): Configured application.properties

---
+++ src/main/java/com/traumaticevolutions/tevosales_backend/TevosalesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.traumaticevolutions.tevosales_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TevosalesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TevosalesBackendApplication.class, args);
+	}
+
+}
